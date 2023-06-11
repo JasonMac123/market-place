fa1cebb43a3e4aee80d815ca201c5006817a9629
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-11 19:46:48 -0400

    Adds SearchBar styling

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 const SearchBar = () => {
-  return <div></div>;
+  return (
+    <div className="rounded-xl bg-white border-[1px] shadow-md">Search!</div>
+  );
 };
 
 export default SearchBar;
