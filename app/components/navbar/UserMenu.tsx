7036153e3814781f1422480c3851c0d476f58209
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-12 15:21:34 -0400

    Adds toggle function to open menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 
 const UserMenu = () => {
   const [openStatus, setOpenStatus] = useState(false);
 
+  const toggleOpen = useCallback(() => {
+    setOpenStatus(!openStatus);
+  }, []);
+
   return (
-    <div className="flex items-center gap-8 rounded-xl bg-white border-[1px] shadow-md">
-      <AiOutlineMenu />
-      <Avatar />
+    <div>
+      <div
+        className="flex items-center gap-8 rounded-xl bg-white border-[1px] shadow-md"
+        onClick={toggleOpen}
+      >
+        <AiOutlineMenu />
+        <Avatar />
+      </div>
     </div>
   );
 };
