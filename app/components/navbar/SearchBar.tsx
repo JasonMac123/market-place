3f929a1ade3cd6e63d9226492c7b7b3e1a612728
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-13 15:24:58 -0400

    Styles search bar

--- app/components/navbar/SearchBar.tsx
+++ app/components/navbar/SearchBar.tsx
@@ -3,9 +3,9 @@ import { RxMagnifyingGlass } from "react-icons/rx";
 
 const SearchBar = () => {
   return (
-    <div className="rounded-xl bg-white border-[1px] shadow-md">
-      <div>Search!</div>
-      <RxMagnifyingGlass />
+    <div className="rounded-full bg-white border-[1px] shadow-md flex justify-between items-center py-8 px-8 h-1/4 w-96">
+      <div className="text-2xl">Search!</div>
+      <RxMagnifyingGlass size={30} />
     </div>
   );
 };
