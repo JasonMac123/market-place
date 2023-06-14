9e33ac2deffc9d45332c8925986f431fd7753851
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-14 16:25:38 -0400

    Populates login and register pages

---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+const LoginPage = () => {
+  return;
+  <div></div>;
+};
+
+export default LoginPage;
---
+++ app/register/page.tsx
@@ -0,0 +1,6 @@
+const RegisterPage = () => {
+  return;
+  <div></div>;
+};
+
+export default RegisterPage;
