a97e53b2f886f0e8dd39c7fb3b7310c14ae6706e
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-16 15:36:38 -0400

    Adds footer content

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import Logo from "../Logo";
+
 const Footer = () => {
-  return <div className="fixed bottom-0 w-full"></div>;
+  return (
+    <div className="fixed bottom-0 w-full h-40 bg-blue-400 flex flex-col py-4 justify-space-around items-center">
+      <div>Follow us on our social media!</div>
+      <div className="flex"></div>
+      <div>@ 2023 - Jason Mac</div>
+    </div>
+  );
 };
 
 export default Footer;
