fb5ea26a1a2c4a43d7ec8a51d64ad8dfd461b3be
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-17 21:27:49 -0400

    Adds transition effects

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -18,14 +18,14 @@ const UserMenu = () => {
   return (
     <div className="relative">
       <div
-        className="flex items-center justify-center gap-8 rounded-2xl bg-white border-[1px] shadow-md mr-8 w-32 py-4"
+        className="flex items-center justify-center gap-8 rounded-2xl bg-neutral-100 border-[1px] shadow-md mr-8 w-32 py-4 hover:bg-neutral-300 transition-all"
         onClick={toggleOpen}
       >
         <AiOutlineMenu />
         <Avatar />
       </div>
       {openStatus && (
-        <div className="absolute right-12 top-20 text-md rounded-md bg-neutral-200 py-8 px-4 border-[2px]">
+        <div className="absolute right-12 top-20 text-md rounded-md bg-neutral-100 py-8 px-4 border-[2px]">
           <div className="flex flex-col items-center justify-center space-y-2">
             <MenuItem
               label="Sign In"
