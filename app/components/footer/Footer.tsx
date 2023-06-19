260e92b425354061386211e5cbc33f1e40128e4b
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-19 15:01:34 -0400

    Changes medialink component styling

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Logo from "../Logo";
+import MediaLink from "./MediaLink";
+import { AiFillFacebook } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full h-40 bg-blue-400 flex flex-col py-4 justify-space-around items-center">
       <div>Follow us on our social media!</div>
-      <div className="flex"></div>
+      <div className="flex">
+        <MediaLink Icon={AiFillFacebook} label="Our FaceBook" />
+      </div>
       <div>@ 2023 - Jason Mac</div>
     </div>
   );
