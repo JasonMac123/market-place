4a3e322938391a83f0308e9bf6b914b31f724505
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-21 18:14:27 -0400

    Changes text-color to become more readable

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -12,13 +12,12 @@ import {
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full h-88 bg-blue-400 flex flex-col py-12 justify-around">
-      <h2 className="text-5xl text-center">Look of Japan!</h2>
-      <div className="flex justify-around">
+    <div className="fixed bottom-0 w-full h-88 bg-blue-400 flex flex-col pt-12 justify-around text-white">
+      <div className="flex justify-around pb-8">
         <div className="flex">
           <Logo alt />
-          <div className="flex flex-col gap-4 justify-start">
-            <h3 className="text-lg">Visit us at our physical stores</h3>
+          <div className="flex flex-col gap-6 justify-start">
+            <h3 className="text-4xl">Visit us at our physical stores</h3>
             <StoreLocation
               streetName="Steeles Avenue West"
               streetNumber={4001}
@@ -33,24 +32,19 @@ const Footer = () => {
               region="British Columbia"
               postalCode="V5T 1Z7"
             />
-            <h3 className="">More coming soon!</h3>
+            <h3 className="text-white text-2xl">More coming soon!</h3>
           </div>
         </div>
         <div>
-          <div>Contact us!</div>
-          <div>
-            <h3>Phone Number</h3>
-            <h3>(416)-111-1111</h3>
+          <div className="text-4xl mb-2">Contact us!</div>
+          <div className="space-y-2">
+            <h3>Phone Number : (416) - 111 - 1111</h3>
+            <h3>Email : Jason@LookOfJapan.com</h3>
           </div>
-          <div>
-            <div>Email</div>
-            <h3>Jason@LookOfJapan.com</h3>
-          </div>
-          <div>Phone Number!</div>
         </div>
-        <div className="flex flex-col items-center">
-          <div className="text-lg">Follow us on our social media!</div>
-          <div>
+        <div className="flex flex-col">
+          <div className="text-4xl mb-4">Follow our social media!</div>
+          <div className="space-y-3">
             <MediaLink Icon={AiFillFacebook} label="FaceBook" />
             <MediaLink Icon={AiFillInstagram} label="Instagram" />
             <MediaLink Icon={AiOutlineTwitter} label="Twitter" />
@@ -58,10 +52,10 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="flex">
-        <div className="mr-4 text-gray-600">Terms</div>
-        <div className="mr-4 text-gray-600">Privacy</div>@ 2023 - Jason Mac All
-        rights reserved
+      <div className="flex justify-end w-full pr-4">
+        <div className="mr-4 text-gray-600 hover:underline">Terms</div>
+        <div className="mr-4 text-gray-600 hover:underline">Privacy</div>@ 2023
+        - Jason Mac All rights reserved
       </div>
     </div>
   );
