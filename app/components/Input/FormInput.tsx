9de0b243c1d2361682861c2450e3487b2b3ad1db
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-22 15:47:03 -0400

    Adds Input to form input to take values

--- app/components/Input/FormInput.tsx
+++ app/components/Input/FormInput.tsx
@@ -1,7 +1,37 @@
 "use client";
 
-const FormInput = () => {
-  return <div></div>;
+import { FieldValues, FieldErrors, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
+
+interface FormInputProps {
+  id: string;
+  label: string;
+  type?: string;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+  id,
+  label,
+  type,
+  formatPrice,
+  required,
+  register,
+  errors,
+}) => {
+  return (
+    <div className="w-full relative">
+      <input
+        id={id}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+      />
+    </div>
+  );
 };
 
 export default FormInput;
