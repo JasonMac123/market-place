a1e21a3dd028c32f5d8948f2dbd4d4193627fa48
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-23 15:40:42 -0400

    Adds input forms for email and password in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,38 @@
 import Container from "../components/Container";
 import FormInput from "../components/Input/FormInput";
 
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <Container>
       <div>
         <div>
-          <FormInput />
+          <FormInput
+            id="email"
+            label="Email"
+            register={register}
+            errors={errors}
+            required
+          />
+          <FormInput
+            id="password"
+            label="Password"
+            register={register}
+            errors={errors}
+            required
+          />
         </div>
       </div>
     </Container>
