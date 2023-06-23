00d144418bf35a236f9beab40bee0f9d396afcc9
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-23 15:30:09 -0400

    Adds styling to label and input

--- app/components/Input/FormInput.tsx
+++ app/components/Input/FormInput.tsx
@@ -29,7 +29,19 @@ const FormInput: React.FC<FormInputProps> = ({
         {...register(id, { required })}
         placeholder=" "
         type={type}
+        className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed 
+        ${formatPrice ? "pl-8" : "pl-4"}
+        ${errors[id] ? "border-rose-600" : "border-slate-300"} 
+        ${errors[id] ? "focus:border-rose-600" : "focus:border-black"}`}
       />
+      <label
+        className={`absolute text-sm duration-150 transform -translate-y-3 top-5 z-10 origin-[0] 
+        ${formatPrice ? "left-9" : "left-4"} 
+        ${errors[id] ? "text-rose-600" : ""}
+        peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4`}
+      >
+        {label}
+      </label>
     </div>
   );
 };
