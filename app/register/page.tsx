58e6d4f4e8c2454a41085a82ea3be27b70b18322
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-24 20:40:07 -0400

    Adds form input for register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,44 @@
+import ClientContainer from "../components/ClientContainer";
+import Container from "../components/Container";
+import FormInput from "../components/Input/FormInput";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
 const RegisterPage = () => {
-  return;
-  <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <ClientContainer>
+      <Container>
+        <div>
+          <div>
+            <FormInput
+              id="email"
+              label="Email"
+              register={register}
+              errors={errors}
+              required
+            />
+            <FormInput
+              id="password"
+              label="Password"
+              register={register}
+              errors={errors}
+              required
+            />
+          </div>
+        </div>
+      </Container>
+    </ClientContainer>
+  );
 };
 
 export default RegisterPage;
