07341fb15957089a9e80e9a691d39d410394e2a8
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-29 15:06:54 -0400

    Fixes client error of useAuthState

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -3,8 +3,14 @@
 import Logo from "../Logo";
 import SearchBar from "./SearchBar";
 import UserMenu from "./UserMenu";
+import firebase_app from "@/app/firebase/config";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 
 const NavBar = () => {
+  const auth = getAuth(firebase_app);
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <div className="fixed flex justify-between items-center bg-blue-500 w-full h-[20vh]">
       <Logo />
