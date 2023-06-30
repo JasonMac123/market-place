ed80d30171b4638dc271dfa67e6a0be35acfc6ba
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-30 15:57:43 -0400

    Adds basic info for marketplace card

--- app/components/FormCard.tsx
+++ app/components/FormCard.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const InfoCard = () => {
-  return <div></div>;
+  return (
+    <div className="w-1/2 h-full bg-celestial flex items-center justify-center">
+      <h2 className="text-alice text-3xl">Sign up for the best deals!</h2>
+    </div>
+  );
 };
 
 export default InfoCard;
