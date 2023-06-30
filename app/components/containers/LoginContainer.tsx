57f8087545a337591b383887defbff478cacbf54
JasonSnow123 <m.jason.2018@gmail.com>
2023-06-30 15:59:22 -0400

    Styles LoginContainer with additional content and adds redirection to register

--- app/components/containers/LoginContainer.tsx
+++ app/components/containers/LoginContainer.tsx
@@ -9,6 +9,7 @@ import { FcGoogle } from "react-icons/fc";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
 import signIn from "@/app/firebase/auth/signin";
+import { useCallback } from "react";
 
 const LoginContainer = () => {
   const {
@@ -43,8 +44,16 @@ const LoginContainer = () => {
     return router.push("/");
   };
 
+  const redirectRegister = useCallback(() => {
+    router.push("/register");
+  }, []);
+
   return (
-    <div className="w-1/2 px-20 space-y-4">
+    <div className="w-1/2 h-full px-20 space-y-6 relative flex flex-col items-center justify-center">
+      <h2 className="text-6xl mb-12">Welcome Back!</h2>
+      <h3 className="text-md">
+        Please enter your details and get ready to shop!
+      </h3>
       <FormInput
         id="email"
         type="email"
@@ -65,11 +74,26 @@ const LoginContainer = () => {
         label={"Login using your account!"}
         onClick={handleSubmit(signInWithNative)}
       />
+      <div className="w-full flex items-center justify-center gap-4">
+        <div className="border-[1px] w-1/3 h-[1px] border-neutral-500"></div>
+        <h4>or</h4>
+        <div className="border-[1px] w-1/3 h-[1px] border-neutral-500"></div>
+      </div>
+
       <Button
         label={"Login with Google!"}
         Icon={FcGoogle}
         onClick={signInWithGoogle}
       />
+      <div className="flex absolute bottom-3 left-[5.5rem] gap-2 text-gray-600">
+        <h4>Don't have an account?</h4>
+        <h4
+          onClick={redirectRegister}
+          className="cursor-pointer hover:underline hover:text-gray-400 transition"
+        >
+          Register with us!
+        </h4>
+      </div>
     </div>
   );
 };
