f6cf765c2e1c2ec7ae91e29e988506bb6b81302f
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-03 17:48:32 -0400

    Changes usermenu if logged or not, and allows user to logout!

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -3,14 +3,8 @@
 import Logo from "../Logo";
 import SearchBar from "./SearchBar";
 import UserMenu from "./UserMenu";
-import firebase_app from "@/app/firebase/config";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { getAuth } from "firebase/auth";
 
 const NavBar = () => {
-  const auth = getAuth(firebase_app);
-  const [user, loading, error] = useAuthState(auth);
-
   return (
     <div className="fixed z-30 flex justify-between items-center bg-lapis w-full h-[20vh]">
       <Logo />
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,19 +1,37 @@
 "use client";
 
 import { useCallback, useState } from "react";
-import { AiOutlineMenu, AiOutlineProfile } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineProfile,
+  AiOutlineShoppingCart,
+  AiOutlineHistory,
+  AiOutlineLogout,
+} from "react-icons/ai";
 import { FaDoorOpen } from "react-icons/fa";
+
 import { useRouter } from "next/navigation";
+import { getAuth } from "@firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase_app from "@/app/firebase/config";
+
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
-  const [openStatus, setOpenStatus] = useState(false);
   const router = useRouter();
+  const auth = getAuth(firebase_app);
+
+  const [user, loading, error] = useAuthState(auth);
+  const [openStatus, setOpenStatus] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setOpenStatus((value) => !value);
   }, []);
+
+  const logOut = useCallback(() => {
+    auth.signOut();
+  }, [auth]);
 
   return (
     <div className="relative">
@@ -26,24 +44,60 @@ const UserMenu = () => {
       </div>
       {openStatus && (
         <div className="absolute right-12 top-20 text-md rounded-md bg-neutral-100 py-8 px-4 border-[2px]">
-          <div className="flex flex-col items-center justify-center space-y-2">
-            <MenuItem
-              label="Sign In"
-              onClick={() => {
-                toggleOpen();
-                router.push("/login");
-              }}
-              Icon={FaDoorOpen}
-            />
-            <hr className="border-black w-3/4" />
-            <MenuItem
-              label="Register"
-              onClick={() => {
-                toggleOpen();
-                router.push("/register");
-              }}
-              Icon={AiOutlineProfile}
-            />
+          <div
+            className="flex flex-col items-center justify-center space-y-2 w-40
+            "
+          >
+            {!user ? (
+              <>
+                <MenuItem
+                  label="Sign In"
+                  onClick={() => {
+                    toggleOpen();
+                    router.push("/login");
+                  }}
+                  Icon={FaDoorOpen}
+                />
+                <hr className="border-black w-3/4" />
+                <MenuItem
+                  label="Register"
+                  onClick={() => {
+                    toggleOpen();
+                    router.push("/register");
+                  }}
+                  Icon={AiOutlineProfile}
+                />
+              </>
+            ) : (
+              <>
+                <MenuItem
+                  label="My Cart"
+                  onClick={() => {
+                    toggleOpen();
+                    router.push("/cart");
+                  }}
+                  Icon={AiOutlineShoppingCart}
+                />
+                <hr className="border-black w-3/4" />
+                <MenuItem
+                  label="My past orders"
+                  onClick={() => {
+                    toggleOpen();
+                    router.push("/orders");
+                  }}
+                  Icon={AiOutlineHistory}
+                />
+                <hr className="border-black w-3/4" />
+                <MenuItem
+                  label="Logout"
+                  onClick={() => {
+                    toggleOpen();
+                    logOut();
+                  }}
+                  Icon={AiOutlineLogout}
+                />
+              </>
+            )}
           </div>
         </div>
       )}
