99b3a506cca89fc4e77231d624ef788fb5445685
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-05 17:46:39 -0400

    Adds store navbar to help filter page

--- app/components/storenavbar/StoreNavBar.tsx
+++ app/components/storenavbar/StoreNavBar.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 const StoreNavBar = () => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <div className="flex">
+        <div>Food</div>
+        <div>Clothing</div>
+        <div>Stationary</div>
+        <div>Kitchenware</div>
+      </div>
+      <div className="flex">
+        <AiOutlineShoppingCart size={20} />
+        Number
+      </div>
+    </div>
+  );
 };
 
 export default StoreNavBar;
