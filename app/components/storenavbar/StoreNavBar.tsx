111e8a6c88d64784458451d4a1f71d649bd350ff
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-05 17:48:13 -0400

    Adds redirection to cart page

--- app/components/storenavbar/StoreNavBar.tsx
+++ app/components/storenavbar/StoreNavBar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useRouter } from "next/navigation";
 
 const StoreNavBar = () => {
+  const router = useRouter();
+
   return (
     <div className="flex">
       <div className="flex">
@@ -11,7 +14,7 @@ const StoreNavBar = () => {
         <div>Stationary</div>
         <div>Kitchenware</div>
       </div>
-      <div className="flex">
+      <div className="flex" onClick={() => router.push("/cart")}>
         <AiOutlineShoppingCart size={20} />
         Number
       </div>
