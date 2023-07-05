e99cab5c65f653b4bd568b6a53f5cbac7f2f63e2
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-05 18:07:34 -0400

    Adds styling to store navbar

--- app/components/storenavbar/StoreNavBar.tsx
+++ app/components/storenavbar/StoreNavBar.tsx
@@ -7,8 +7,8 @@ const StoreNavBar = () => {
   const router = useRouter();
 
   return (
-    <div className="flex">
-      <div className="flex">
+    <div className="flex w-3/4 h-20 mx-auto items-center justify-between bg-cerulean">
+      <div className="flex gap-4 items-center">
         <div onClick={() => router.push("/?category=food")}>Food</div>
         <div onClick={() => router.push("/?category=clothing")}>Clothing</div>
         <div onClick={() => router.push("/?category=stationary")}>
@@ -16,7 +16,10 @@ const StoreNavBar = () => {
         </div>
         <div onClick={() => router.push("/?category=kitchen")}>Kitchenware</div>
       </div>
-      <div className="flex" onClick={() => router.push("/cart")}>
+      <div
+        className="flex space-x-4 items-center"
+        onClick={() => router.push("/cart")}
+      >
         <AiOutlineShoppingCart size={20} />
         Number
       </div>
