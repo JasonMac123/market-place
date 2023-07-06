801bb98f403e222ac23b29eacd6c8c4225e88e89
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-06 15:14:21 -0400

    Include all props given into interface

--- app/components/cards/ItemCard.tsx
+++ app/components/cards/ItemCard.tsx
@@ -1,11 +1,30 @@
 "use client";
 
 interface ItemCardProps {
-  id: string;
-  label: string;
+  id: string | number;
+  label: string | number;
+  description: string | number;
+  image: string | number;
+  quantity: string | number;
+  region: string | number;
+  maker: string | number;
+  rating: string | number;
+  price: string | number;
+  title: string | number;
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ id, label }) => {
+const ItemCard: React.FC<ItemCardProps> = ({
+  id,
+  label,
+  description,
+  image,
+  quantity,
+  region,
+  maker,
+  rating,
+  price,
+  title,
+}) => {
   return <div>{label}</div>;
 };
 
