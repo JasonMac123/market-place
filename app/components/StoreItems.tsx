b505f7932406ef08b07f4b5cf79e80062769d8fd
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-06 15:01:45 -0400

    Passes down all item details to itemcard

--- app/components/StoreItems.tsx
+++ app/components/StoreItems.tsx
@@ -10,7 +10,18 @@ const StoreItems = async () => {
   return (
     <div>
       {items.map((item: Item) => {
-        <ItemCard id={item.id} label={item.name} />;
+        <ItemCard
+          id={item.id}
+          label={item.name}
+          description={item.description}
+          image={item.image}
+          quantity={item.quantity}
+          region={item.region}
+          maker={item.maker}
+          rating={item.rating}
+          price={item.price}
+          title={item.title}
+        />;
       })}
     </div>
   );
