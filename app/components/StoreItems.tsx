059bdf89519c61b511db10ea57d646eae8fc30eb
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-06 15:21:06 -0400

    Fixes error where itemcard would not show

--- app/components/StoreItems.tsx
+++ app/components/StoreItems.tsx
@@ -10,18 +10,20 @@ const StoreItems = async () => {
   return (
     <div>
       {items.map((item: Item) => {
-        <ItemCard
-          id={item.id}
-          label={item.name}
-          description={item.description}
-          image={item.image}
-          quantity={item.quantity}
-          region={item.region}
-          maker={item.maker}
-          rating={item.rating}
-          price={item.price}
-          name={item.name}
-        />;
+        return (
+          <ItemCard
+            id={item.id}
+            label={item.name}
+            description={item.description}
+            image={item.image}
+            quantity={item.quantity}
+            region={item.region}
+            maker={item.maker}
+            rating={item.rating}
+            price={item.price}
+            name={item.name}
+          />
+        );
       })}
     </div>
   );
