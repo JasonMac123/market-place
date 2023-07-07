db03acd80196a1d0a71a6c2b5a4290068de81b03
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-07 15:55:26 -0400

    creates categories using array returning component

--- app/components/storenavbar/StoreNavBar.tsx
+++ app/components/storenavbar/StoreNavBar.tsx
@@ -5,13 +5,14 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { LuUtensilsCrossed } from "react-icons/lu";
 import { FaTshirt, FaPenFancy } from "react-icons/fa";
 import { MdKitchen } from "react-icons/md";
+import StoreCategory from "./StoreCategory";
 
 const categories = [
   { label: "Food", Icon: LuUtensilsCrossed },
   { label: "Clothing", Icon: FaTshirt },
   { label: "Stationary", Icon: FaPenFancy },
   { label: "Kitchen-Ware", Icon: MdKitchen },
-  { label: "All" },
+  { label: "All items" },
 ];
 
 const StoreNavBar = () => {
@@ -21,7 +22,18 @@ const StoreNavBar = () => {
 
   return (
     <div className="flex w-3/4 h-20 mx-auto items-center justify-between bg-celestial px-12 rounded-lg">
-      <div className="flex gap-4 w-1/2 h-full items-center"></div>
+      <div className="flex gap-4 w-1/2 h-full items-center">
+        {categories.map((item) => {
+          return (
+            <StoreCategory
+              key={item.label}
+              label={item.label}
+              selected={category === item.label}
+              Icon={item.Icon}
+            />
+          );
+        })}
+      </div>
       <div
         className="flex items-center text-alice gap-4 hover:bg-cerulean h-3/4 px-4"
         onClick={() => router.push("/cart")}
