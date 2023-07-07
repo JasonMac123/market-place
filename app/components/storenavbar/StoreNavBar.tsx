0f70ac7ac0ccbb5151e4c4551f588ef4f8413efa
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-07 15:15:28 -0400

    Adds on hover effect for cart

--- app/components/storenavbar/StoreNavBar.tsx
+++ app/components/storenavbar/StoreNavBar.tsx
@@ -10,32 +10,32 @@ const StoreNavBar = () => {
     <div className="flex w-3/4 h-20 mx-auto items-center justify-between bg-celestial px-12 rounded-lg">
       <div className="flex gap-4 w-1/2 h-full items-center">
         <div
-          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center"
+          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center px-4"
           onClick={() => router.push("/?category=food")}
         >
           Food
         </div>
         <div
-          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center"
+          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center px-4"
           onClick={() => router.push("/?category=clothing")}
         >
           Clothing
         </div>
         <div
-          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center"
+          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center px-4"
           onClick={() => router.push("/?category=stationary")}
         >
           Stationary
         </div>
         <div
-          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center"
+          className=" text-alice text-lg hover:bg-cerulean h-3/4 flex items-center px-4"
           onClick={() => router.push("/?category=kitchen")}
         >
           Kitchenware
         </div>
       </div>
       <div
-        className="flex items-center text-alice gap-4"
+        className="flex items-center text-alice gap-4 hover:bg-cerulean h-3/4 px-4"
         onClick={() => router.push("/cart")}
       >
         Number
