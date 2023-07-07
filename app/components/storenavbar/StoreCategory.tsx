8c35fd40b471a14a4eb3b79ec4916b21782361a9
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-07 15:39:34 -0400

    Adds category icons and changes bg colour if selected

--- app/components/storenavbar/StoreCategory.tsx
+++ app/components/storenavbar/StoreCategory.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import queryString from "query-string";
 
 interface StoreCategoryProps {
   label: string;
@@ -8,10 +12,47 @@ interface StoreCategoryProps {
   selected?: Boolean;
 }
 
+interface Query {
+  [name: string]: string;
+}
+
 const StoreCategory: React.FC<StoreCategoryProps> = ({
   Icon,
   label,
   selected,
-}) => {};
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const updatedQuery: Query = {
+      category: label,
+    };
+
+    const url = queryString.stringifyUrl(
+      { url: "/", query: updatedQuery },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [router, label, params]);
+
+  return (
+    <div
+      className={`text-alice text-lg hover:bg-cerulean h-3/4 flex items-center px-4 
+      ${selected ? "bg-cerulean" : ""}`}
+      onClick={handleClick}
+    >
+      <Icon size={20} />
+      {label}
+    </div>
+  );
+};
 
 export default StoreCategory;
