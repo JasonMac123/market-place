88dfc660c542634d5f7337bc11877cdfcb5336fd
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-08 15:36:10 -0400

    Server retrival of searchParams instead of client

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@ import ClientContainer from "./components/containers/ClientContainer";
 import Container from "./components/containers/Container";
 import StoreItems from "./components/StoreItems";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: Query;
+}
+
+interface Query {
+  category: string;
+}
+
+export default function Home({ searchParams }: HomeProps) {
   return (
     <Container>
       <ClientContainer>
-        <StoreItems />
+        <StoreItems searchParams={searchParams} />
       </ClientContainer>
     </Container>
   );
