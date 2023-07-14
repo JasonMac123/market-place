6e6a20ba3f6946813f72745068941f0c58d0ae36
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-13 22:06:31 -0400

    Create item inputs to show item stats

---
+++ app/components/itemdisplays/ItemInputs.tsx
@@ -0,0 +1,17 @@
+interface ItemInputsProps {
+  quantity: number;
+  price: number;
+  rating: number;
+  options: [];
+}
+
+const ItemInputs: React.FC<ItemInputsProps> = ({
+  quantity,
+  price,
+  rating,
+  options,
+}) => {
+  return <div></div>;
+};
+
+export default ItemInputs;
