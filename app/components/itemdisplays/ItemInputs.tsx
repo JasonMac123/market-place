f409a59cb1f9e2bc33d8229c14f003b758c6c6f5
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-13 22:10:24 -0400

    Removes rating from input to change design layout

--- app/components/itemdisplays/ItemInputs.tsx
+++ app/components/itemdisplays/ItemInputs.tsx
@@ -1,19 +1,16 @@
 interface ItemInputsProps {
   quantity: number;
   price: number;
-  rating: number;
   options: [];
 }
 
 const ItemInputs: React.FC<ItemInputsProps> = ({
   quantity,
   price,
-  rating,
   options,
 }) => {
   return (
     <div className="border-[1px]">
-      <h3>{rating}</h3>
       <h3>{quantity}</h3>
       <h3>{price}</h3>
     </div>
