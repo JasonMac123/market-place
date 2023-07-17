897b00a75900246849837502c495852960c3075b
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-17 19:05:30 -0400

    Styles button potentially if item is out of stock

--- app/components/Input/Button.tsx
+++ app/components/Input/Button.tsx
@@ -6,13 +6,17 @@ interface ButtonProps {
   label: string;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   Icon?: IconType;
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, Icon }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, Icon, disabled }) => {
   return (
     <button
       onClick={onClick}
-      className="relative rounded-lg hover:opacity-80 transition w-full py-4 font-semibold border-2 cursor-pointer bg-green-400 border-green-400 text-black"
+      className={`relative rounded-lg hover:opacity-80 transition w-full py-4 font-semibold border-2 cursor-pointer  text-black
+      ${
+        disabled ? "bg-red-400 border-red-400" : "bg-green-400 border-green-400"
+      }`}
     >
       {Icon && <Icon size={24} className="absolute left-3 top-3" />}
       {label}
