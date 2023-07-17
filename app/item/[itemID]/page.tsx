a4c8cd2136d35ca458d882d94e46c5e0b099f516
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-16 22:01:56 -0400

    Fixes hydration errors for client components in individual item page

--- app/item/[itemID]/page.tsx
+++ app/item/[itemID]/page.tsx
@@ -1,4 +1,5 @@
 import DataError from "@/app/components/DataError";
+import ClientContainer from "@/app/components/containers/ClientContainer";
 import ItemFront from "@/app/components/itemdisplays/ItemFront";
 import ItemInputs from "@/app/components/itemdisplays/ItemInputs";
 
@@ -16,29 +17,38 @@ const ItemPage = async ({ params }: { params: itemParams }) => {
   if (!item) {
     return (
       <div>
-        <DataError />
+        <ClientContainer>
+          <DataError />
+        </ClientContainer>
       </div>
     );
   }
 
   return (
     <div className="flex w-3/4 mx-auto py-12 gap-8">
-      <Image src={item.image} alt="picture of item" width={600} height={1000} />
-      <div className="flex flex-col gap-4 bg-white border-[1px] rounded-lg">
-        <ItemFront
-          name={item.name}
-          description={item.description}
-          region={item.region}
-          rating={item.rating}
-          maker={item.maker}
+      <ClientContainer>
+        <Image
+          src={item.image}
+          alt="picture of item"
+          width={600}
+          height={1000}
         />
-        <ItemInputs
-          quantity={item.quantity}
-          price={item.price}
-          options={item.options}
-          id={params.itemID}
-        />
-      </div>
+        <div className="flex flex-col gap-4 bg-white border-[1px] rounded-lg">
+          <ItemFront
+            name={item.name}
+            description={item.description}
+            region={item.region}
+            rating={item.rating}
+            maker={item.maker}
+          />
+          <ItemInputs
+            quantity={item.quantity}
+            price={item.price}
+            options={item.options}
+            id={params.itemID}
+          />
+        </div>
+      </ClientContainer>
     </div>
   );
 };
