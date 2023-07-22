689ae565b4537d31003be99531a86a0e57eedb84
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-22 19:02:38 -0400

    Creates error page for when an user is not logged in

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,12 @@
+import { BiErrorCircle } from "react-icons/bi";
+
 const Page = () => {
-  return <div></div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <BiErrorCircle />
+      <h3>Please log in to see your cart information</h3>
+    </div>
+  );
 };
 
 export default Page;
