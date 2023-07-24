3315179a89c30f1e4012ac73024b83fc46b376dd
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-23 20:02:21 -0400

    Styles error page for cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,8 +3,10 @@ import { BiErrorCircle } from "react-icons/bi";
 const Page = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <BiErrorCircle />
-      <h3>Please log in to see your cart information</h3>
+      <div className="fle items-center justify-center border-[1px] p-8">
+        <BiErrorCircle />
+        <h3 className="text-3xl">Please log in to see your cart information</h3>
+      </div>
     </div>
   );
 };
