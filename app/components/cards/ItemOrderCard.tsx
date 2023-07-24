6e6078e717e5d4ae4221b99622f23f857275155e
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-23 20:26:53 -0400

    Adds item content into item order card

--- app/components/cards/ItemOrderCard.tsx
+++ app/components/cards/ItemOrderCard.tsx
@@ -1,5 +1,40 @@
-const ItemOrderCard = () => {
-  return <div></div>;
+"use client";
+
+import Image from "next/image";
+
+interface ItemOrderCardProps {
+  itemID: string;
+  optionType: Option;
+  orderAmount: number;
+  image: string;
+  itemName: string;
+  amount: number;
+}
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+const ItemOrderCard: React.FC<ItemOrderCardProps> = ({
+  itemID,
+  optionType,
+  orderAmount,
+  image,
+  amount,
+  itemName,
+}) => {
+  return (
+    <div className="flex">
+      <Image src={image} alt="picture of product" width={200} height={200} />
+      <div>
+        <h3>{itemName}</h3>
+        <h3>{optionType.label}</h3>
+        <h3>{amount}</h3>
+        <h3>{orderAmount}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default ItemOrderCard;
