389fcbb32ce0f549170ba2206df36dc5c62c2859
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-24 15:09:37 -0400

    Updates cart page to show item total amount

--- app/cart/[userID]/page.tsx
+++ app/cart/[userID]/page.tsx
@@ -9,12 +9,22 @@ interface UserParams {
 const Page = async ({ params }: { params: UserParams }) => {
   const userCart = await getUserCart(params);
 
+  const totalAmount = userCart.reduce((acc, value) => value.amount + acc, 0);
+
   return (
     <div className="mx-20 my-4 p-4">
       <h1>Cart</h1>
       {userCart.map((item) => {
         return <ItemOrderCard {...item} />;
       })}
+      <div className="flex jusitfy-end">
+        <div>Your Subtotal: {totalAmount}</div>
+        <div>Your tax: {totalAmount * 0.13}</div>
+        <div>
+          Your Total including tax (GST + HST):
+          {totalAmount + totalAmount * 0.13}
+        </div>
+      </div>
     </div>
   );
 };
