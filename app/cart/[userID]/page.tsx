0e1a48fb63ef2967bd5472df2f9b76d246e057fb
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-24 15:28:16 -0400

    Fixes hydration errors and styles columns

--- app/cart/[userID]/page.tsx
+++ app/cart/[userID]/page.tsx
@@ -1,6 +1,7 @@
 import getUserCart from "@/app/firebase/getUserCart";
 
 import ItemOrderCard from "@/app/components/cards/ItemOrderCard";
+import ClientContainer from "@/app/components/containers/ClientContainer";
 
 interface UserParams {
   userID: string;
@@ -12,16 +13,18 @@ const Page = async ({ params }: { params: UserParams }) => {
   const totalAmount = userCart.reduce((acc, value) => value.amount + acc, 0);
 
   return (
-    <div className="mx-20 my-4 p-4">
+    <div className="mx-auto my-4 p-4 w-3/4">
       <h1>Cart</h1>
-      <div className="flex">
-        <h2>Image</h2>
-        <h2>Description</h2>
-        <h2>Price</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="text-3xl">Image</h2>
+        <h2 className="text-3xl">Description</h2>
+        <h2 className="text-3xl">Price</h2>
       </div>
-      {userCart.map((item) => {
-        return <ItemOrderCard {...item} />;
-      })}
+      <ClientContainer>
+        {userCart.map((item) => {
+          return <ItemOrderCard {...item} />;
+        })}
+      </ClientContainer>
       <div className="flex jusitfy-end">
         <div>Your Subtotal: {totalAmount}</div>
         <div>Your tax: {totalAmount * 0.13}</div>
