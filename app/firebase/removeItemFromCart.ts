07f6b0b6bf0a4ae5f5e89b4e3650d22d160c5d50
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-26 15:19:15 -0400

    Create function to filter out items out of cart

--- app/firebase/removeItemFromCart.ts
+++ app/firebase/removeItemFromCart.ts
@@ -1,6 +1,14 @@
 import firebase_app from "./config";
 
-import { collection, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  query,
+  where,
+  getDocs,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
 interface Params {
   userID: UserParams;
@@ -19,6 +27,27 @@ export default async function removeItemFromCart(params: Params) {
 
     const db = getFirestore(firebase_app);
     const cartSnapShot = collection(db, "cart");
+
+    const queryUserCart = await query(cartSnapShot, where("userid", "==", id));
+
+    let cart: any = {};
+    let cartID = "";
+
+    const userCart = await getDocs(queryUserCart);
+    userCart.forEach((doc) => {
+      cart = { ...doc.data() };
+      cartID = doc.id;
+    });
+
+    const newOrderedItemList = cart.items.filter(
+      (item: any) =>
+        item.itemID !== itemID && item.optionType.label !== itemLabel
+    );
+
+    cart.items = newOrderedItemList;
+
+    const docRef = doc(db, "cart", cartID);
+    await setDoc(docRef, cart);
 
     return true;
   } catch (e: any) {
