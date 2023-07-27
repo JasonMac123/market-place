d13666e6f70f31d613588c7bd429964d96768a4e
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-27 15:28:35 -0400

    Fixes async client component runtime errors

--- app/components/storenavbar/StoreCart.tsx
+++ app/components/storenavbar/StoreCart.tsx
@@ -1,15 +1,12 @@
-"use client";
-
 import { getAuth } from "@firebase/auth";
 import firebase_app from "@/app/firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import getUserCart from "@/app/firebase/getUserCart";
 
 import { useRouter } from "next/navigation";
 
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
-const StoreCart = async () => {
+const StoreCart = () => {
   const router = useRouter();
 
   const auth = getAuth(firebase_app);
@@ -21,21 +18,18 @@ const StoreCart = async () => {
         className="flex items-center text-alice gap-4 hover:bg-cerulean h-3/4 px-4 rounded-lg"
         onClick={() => router.push("/cart")}
       >
-        0
+        Cart
         <AiOutlineShoppingCart size={20} />
       </div>
     );
   }
-
-  const cart = await getUserCart({ userID: user.uid });
-  const countedCart = cart.reduce((acc, value) => value.orderAmount + acc, 0);
 
   return (
     <div
       className="flex items-center text-alice gap-4 hover:bg-cerulean h-3/4 px-4 rounded-lg"
       onClick={() => router.push(`/cart/${user.uid}`)}
     >
-      {countedCart}
+      Cart
       <AiOutlineShoppingCart size={20} />
     </div>
   );
