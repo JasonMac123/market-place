2339e82b47b8e8fb53e3df790e0a384369cab55b
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-27 15:27:34 -0400

    Fixes item description overflowing issues

--- app/components/cards/ItemCard.tsx
+++ app/components/cards/ItemCard.tsx
@@ -11,7 +11,7 @@ interface ItemCardProps {
   maker: string;
   price: number;
   name: string;
-  description: string;
+  shortDescription: string;
 }
 
 const ItemCard: React.FC<ItemCardProps> = ({
@@ -19,31 +19,35 @@ const ItemCard: React.FC<ItemCardProps> = ({
   label,
   image,
   price,
-  description,
+  shortDescription,
 }) => {
   const router = useRouter();
 
   return (
-    <div className="w-1/4 h-[60vh] space-y-4">
-      <div
-        className="w-full h-full flex flex-col p-4 rounded-lg bg-neutral-200 hover:bg-neutral-300 cursor-pointer justify-center items-center"
-        onClick={() => {
-          router.push(`/item/${id}`);
-        }}
-      >
-        <Image
-          src={
-            "https://cb.scene7.com/is/image/Crate/TondoChopsticksSSS22/$web_pdp_main_carousel_med$/211116170028/tondo-chopsticks.jpg"
-          }
-          alt="picture of item"
-          width={400}
-          height={400}
-        />
-        <h3 className="text-lg mt-4 text-left w-full ml-2">{label}</h3>
-        <h4 className="text-mg text-left w-full ml-2 mt-4">{description}</h4>
-        <div className="flex w-full justify-end">
-          <h4 className="text-xl text-green-600">$</h4>
-          <h4 className="text-xl text-green-600">{price}</h4>
+    <div className="w-1/4 h-[70vh] space-y-4">
+      <div className="w-full h-full flex flex-col justify-between items-center bg-neutral-200 hover:bg-neutral-300 cursor-pointer rounded-lg">
+        <div
+          className="w-full h-full flex flex-col p-4 items-center"
+          onClick={() => {
+            router.push(`/item/${id}`);
+          }}
+        >
+          <Image
+            src={
+              "https://cb.scene7.com/is/image/Crate/TondoChopsticksSSS22/$web_pdp_main_carousel_med$/211116170028/tondo-chopsticks.jpg"
+            }
+            alt="picture of item"
+            width={400}
+            height={400}
+          />
+          <h3 className="text-lg mt-4 text-left w-full ml-2">{label}</h3>
+          <h4 className="text-mg text-left w-full ml-2 mt-4">
+            {shortDescription}
+          </h4>
+        </div>
+        <div className="flex w-full justify-end mr-4 mb-4">
+          <h4 className="text-3xl text-green-600">$</h4>
+          <h4 className="text-3xl text-green-600">{price}</h4>
         </div>
       </div>
     </div>
