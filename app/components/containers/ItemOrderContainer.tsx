c07f73f22c1dcb2262478fd6e511c7c618104ec7
JasonSnow123 <m.jason.2018@gmail.com>
2023-07-28 21:41:45 -0400

    Create ItemOrderContainer to hold cart list in state

--- app/components/containers/ItemOrderContainer.tsx
+++ app/components/containers/ItemOrderContainer.tsx
@@ -1,6 +1,26 @@
 "use client";
 
-const ItemOrderContainer = () => {
+interface OptionSelect {
+  label: string;
+  value: string;
+}
+
+interface Item {
+  image: string;
+  itemID: string;
+  itemName: string;
+  optionType: OptionSelect;
+  orderAmount: number;
+  orderQuantity: number;
+}
+
+interface ItemOrderContainerProps {
+  userCart: Item[];
+}
+
+const ItemOrderContainer: React.FC<ItemOrderContainerProps> = ({
+  userCart,
+}) => {
   return <div></div>;
 };
 
