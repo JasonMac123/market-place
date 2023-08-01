e31ceee213dfcc7446559c0538597f4b2e7cdf6b
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-01 15:52:26 -0400

    Creates example stripe checkout to test

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,6 +1,30 @@
 import Stripe from "stripe";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(request) {
-  const stripe = new Stripe(proccess.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-11-15",
+});
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const params: Stripe.Checkout.SessionCreateParams = {
+      submit_type: "donate",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          name: "Chopsticks",
+          amount: 10,
+          quantity: 1,
+          currency: "CAD",
+        },
+      ],
+      success_url: "localhost:3000",
+      cancel_url: "localhost:3000",
+    };
+
+    const checkoutSession: Stripe.Checkout.Session =
+      await stripe.checkout.sessions.create(params);
+  } catch (e: any) {
+    throw new Error(e);
+  }
 }
