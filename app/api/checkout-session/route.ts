cf1e6c4ae82689387cb64749bcc6eb97d0b372bb
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-07 16:52:36 -0400

    Fixes axios and stripe error upon post request details

--- app/api/checkout-session/route.ts
+++ app/api/checkout-session/route.ts
@@ -2,10 +2,6 @@ import Stripe from "stripe";
 import { NextResponse, NextRequest } from "next/server";
 import { Item } from "@/app/types/types";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2022-11-15",
-});
-
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
@@ -19,17 +15,17 @@ export async function POST(req: NextRequest) {
       return { price: item.stripeID, quantity: item.orderQuantity };
     });
 
-    const params: Stripe.Checkout.SessionCreateParams = {
-      submit_type: "donate",
-      payment_method_types: ["card"],
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+      apiVersion: "2022-11-15",
+    });
+
+    const session = await stripe.checkout.sessions.create({
       line_items: newCart,
-      success_url: "localhost:3000",
-    };
+      mode: "payment",
+      success_url: "http://localhost:3000",
+    });
 
-    const checkoutSession: Stripe.Checkout.Session =
-      await stripe.checkout.sessions.create(params);
-
-    return NextResponse.json(checkoutSession.url);
+    return NextResponse.json(session.id);
   } catch (e: any) {
     throw new Error(e);
   }
