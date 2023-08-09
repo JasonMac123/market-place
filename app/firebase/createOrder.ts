35437256464c29eb03222ccbed4d057a0ed95a3e
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-08 21:44:54 -0400

    Creates the order and deletes the cart information

--- app/firebase/createOrder.ts
+++ app/firebase/createOrder.ts
@@ -2,21 +2,44 @@ import { NextResponse } from "next/server";
 import { Item } from "../types/types";
 import firebase_app from "./config";
 
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 interface Params {
   cart: Item[];
   orderNumber: string;
+  userID: string;
 }
 
 export default async function createOrder(params: Params) {
   try {
-    const { cart, orderNumber } = params;
+    const { cart, orderNumber, userID } = params;
 
     const db = getFirestore(firebase_app);
+    const cartSnapShot = collection(db, "cart");
     const orderSnapShot = collection(db, "orders");
 
     let order = await addDoc(orderSnapShot, { order: orderNumber, cart: cart });
+
+    const queryUserCart = await query(
+      cartSnapShot,
+      where("userid", "==", userID)
+    );
+
+    let cartID = "";
+    const userCartInfo = await getDocs(queryUserCart);
+    userCartInfo.forEach((doc) => {
+      cartID = doc.id;
+    });
+    deleteDoc(doc(db, "cart", cartID));
 
     return NextResponse.json(order);
   } catch (e: any) {
