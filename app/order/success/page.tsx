4ce32111b3eacc87972565bc640dfd80468d652f
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-08 21:23:02 -0400

    Uses search params to retrieve session ID from stripe

--- app/order/success/page.tsx
+++ app/order/success/page.tsx
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const OrderPage = () => {
+  const {
+    query: { sessionID },
+  } = useRouter();
+
   return (
     <div className="w-9/11 mx-auto flex justify-center items-center">
       <div className="border-[1px] border-green-300">
