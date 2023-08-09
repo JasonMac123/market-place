c23274a3aca821886002f184c1181bf5f1eef136
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-08 21:25:39 -0400

    Adds session ID number to order number

--- app/order/success/page.tsx
+++ app/order/success/page.tsx
@@ -9,7 +9,7 @@ const OrderPage = () => {
     <div className="w-9/11 mx-auto flex justify-center items-center">
       <div className="border-[1px] border-green-300">
         <h1>Thanks for your order!</h1>
-        <h2>Your order number is #</h2>
+        <h2>Your order number is {sessionID}</h2>
       </div>
     </div>
   );
