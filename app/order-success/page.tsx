6e0c0fe73f17a3d421f17270dd4ad6696e88f896
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-08 20:56:00 -0400

    Creates order success page

---
+++ app/order-success/page.tsx
@@ -0,0 +1,12 @@
+const OrderSuccess = () => {
+  return (
+    <div className="w-9/11 mx-auto flex justify-center items-center">
+      <div className="border-[1px] border-green-300">
+        <h1>Thanks for your order!</h1>
+        <h2>Your order number is #</h2>
+      </div>
+    </div>
+  );
+};
+
+export default OrderSuccess;
