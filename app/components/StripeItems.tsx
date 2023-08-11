504effa898b6470250ec2661c4be9aa80e8d5287
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-11 19:02:40 -0400

    Creates stripe items card to create item cards

---
+++ app/components/StripeItems.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface StripeItemsProps {
+  data: any;
+}
+
+const StripeItems: React.FC<StripeItemsProps> = async ({ data }) => {
+  return <div></div>;
+};
+
+export default StripeItems;
