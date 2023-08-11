24c080f313a35893489b8eae6cd773968d9ff49d
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-11 18:34:43 -0400

    Changes folder organization to redirect user to success page

---
+++ app/order/success/[sessionID]/page.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+interface sessionParams {
+  sessionID: string;
+}
+
+const OrderPage = async ({ params }: { params: sessionParams }) => {
+  const result = await axios.get(
+    `http://localhost:3000/api/session/${params.sessionID}`
+  );
+  console.log(result.data);
+  if (!result.data) {
+    return (
+      <div className="w-9/11 mx-auto flex justify-center items-center">
+        <div className="border-[1px] border-green-300">
+          <h1>Thanks for your order!</h1>
+          <h2>Your order number is {params.sessionID}</h2>
+        </div>
+        <div className="flex flex-col"></div>
+      </div>
+    );
+  }
+  return (
+    <div className="w-9/11 mx-auto flex justify-center items-center">
+      <div className="border-[1px] border-green-300">
+        <h1>Thanks for your order!</h1>
+        <h2>Your order number is {params.sessionID}</h2>
+      </div>
+      <div className="flex flex-col"></div>
+    </div>
+  );
+};
+
+export default OrderPage;
