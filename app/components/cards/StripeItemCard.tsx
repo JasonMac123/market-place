1982f38011307314605958592ca4ee3194708826
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-12 22:10:26 -0400

    Passes stripe item information into the stripe item card

--- app/components/cards/StripeItemCard.tsx
+++ app/components/cards/StripeItemCard.tsx
@@ -1,6 +1,24 @@
 "use client";
 
-const StripeItemCard = () => {
+interface StripeItemCardProps {
+  id: string;
+  object: string;
+  amount_discount: number;
+  amount_subtotal: number;
+  amount_tax: number;
+  amount_total: number;
+  currency: string;
+  description: string;
+  price: Object;
+  quantity: number;
+}
+
+const StripeItemCard: React.FC<StripeItemCardProps> = ({
+  id,
+  amount_total,
+  description,
+  quantity,
+}) => {
   return <div></div>;
 };
 
