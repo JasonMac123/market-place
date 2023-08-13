4a5be19c61aac1678fe11b8a9b1f91e623430b20
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-12 21:57:31 -0400

    Styles stripe item order page when successfully retrieving data

--- app/order/success/[sessionID]/page.tsx
+++ app/order/success/[sessionID]/page.tsx
@@ -24,12 +24,17 @@ const OrderPage = async ({ params }: { params: sessionParams }) => {
   }
   return (
     <ClientContainer>
-      <div className="w-9/11 mx-auto flex justify-center items-center">
-        <div className="border-[1px] border-green-300">
-          <h1>Thanks for your order!</h1>
-          <h2>Your order number is {params.sessionID}</h2>
+      <div className="w-9/12 mx-auto flex flex-col justify-center items-center">
+        <div className="w-full border-[4px] border-green-300 p-8 space-y-4 rounded-lg">
+          <h1 className="text-5xl underline">Thanks for your order!</h1>
+          <h2 className="text-md">
+            Your order number is{" "}
+            <b className="text-lg hover:underline">{params.sessionID}</b>
+          </h2>
         </div>
+        <hr className="border-black w-3/4 my-8" />
         <div className="flex flex-col">
+          <h2 className="text-3xl w-full">Your Items</h2>
           <StripeItems data={result.data.data} />
         </div>
       </div>
