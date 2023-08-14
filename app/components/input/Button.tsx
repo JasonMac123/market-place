0d0be1921dec934fef13abb5d5191293db61565c
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-13 22:41:59 -0400

    Adds refresh ability in client component for button

--- app/components/input/Button.tsx
+++ app/components/input/Button.tsx
@@ -1,18 +1,39 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { IconType } from "react-icons";
 
 interface ButtonProps {
   label: string;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   Icon?: IconType;
   disabled?: boolean;
+  refresh?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick, Icon, disabled }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  Icon,
+  disabled,
+  refresh,
+}) => {
+  let mouseEvent;
+  const router = useRouter();
+
+  const refreshPage = () => {
+    router.refresh();
+  };
+
+  if (refresh) {
+    mouseEvent = refreshPage;
+  } else {
+    mouseEvent = onClick;
+  }
+
   return (
     <button
-      onClick={onClick}
+      onClick={mouseEvent}
       className={`relative rounded-lg hover:opacity-80 transition w-full py-4 font-semibold border-2 cursor-pointer  text-black
       ${
         disabled ? "bg-red-400 border-red-400" : "bg-green-400 border-green-400"
