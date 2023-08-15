d485c2ae61b6b7533748042eb8079554ed90f0bc
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-14 22:38:08 -0400

    Fixes stripe image look up as stripe id changed

--- app/util/convertSIDToImage.ts
+++ app/util/convertSIDToImage.ts
@@ -5,7 +5,7 @@ interface StripeIDImagePair {
 const stripeIDImagePair: StripeIDImagePair = {
   price_1NaNwMJxcB1myVoNTi2ERe6n:
     "https://cb.scene7.com/is/image/Crate/TondoChopsticksSSS22/$web_pdp_main_carousel_med$/211116170028/tondo-chopsticks.jpg",
-  price_1NbuPRJxcB1myVoNFQKh0Vuj:
+  price_1NfCqeJxcB1myVoNpoxZeLcl:
     "https://cb.scene7.com/is/image/Crate/TondoChopsticksSSS22/$web_pdp_main_carousel_med$/211116170028/tondo-chopsticks.jpg",
 };
 
