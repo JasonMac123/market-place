b7df23af48b16a13f99d8e8583c86d38ef4da971
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-14 22:32:12 -0400

    Updated price totals with new stripe data fetch

--- app/components/StripeItems.tsx
+++ app/components/StripeItems.tsx
@@ -9,11 +9,22 @@ interface StripeItemsProps {
 }
 
 const StripeItems: React.FC<StripeItemsProps> = ({ data }) => {
+  const totalPrice =
+    data.reduce((acc, item) => item.amount_subtotal + acc, 0) / 100;
+
+  const totalTax = data.reduce((acc, item) => item.amount_tax + acc, 0) / 100;
   return (
     <>
       {data.map((item) => {
         return <StripeItemCard key={item.id} {...item} />;
       })}
+      <div className="flex flex-col w-full justify-center items-center">
+        <h3>Pricing summary</h3>
+        <div></div>
+        <h4>Subtotal: {totalPrice}</h4>
+        <h4>Tax: {totalTax}</h4>
+        <h4>Total Price: {totalPrice + totalTax}</h4>
+      </div>
     </>
   );
 };
