ccc8abc25711493b5a3592a8a32550e59fe2e04b
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-18 17:58:14 -0400

    Creates a route to retrieve all user order details

---
+++ app/api/multiple-sessions/[userID]/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2022-11-15",
+});
+
+export async function GET(context: any) {
+  try {
+    const userID = context.params.userID;
+    if (!userID) {
+      throw new Error("No session ID specificed");
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.list({
+      customer: userID,
+    });
+
+    return NextResponse.json(checkoutSession);
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
