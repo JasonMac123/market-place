1fa6896b6324dc454f4d20b93e2955fe46dbd59f
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-19 21:43:07 -0400

    Revert changes from param name from customerID to userID

--- app/api/multiple-sessions/[userID]/route.ts
+++ app/api/multiple-sessions/[userID]/route.ts
@@ -7,16 +7,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function GET(context: any) {
   try {
-    const customerID = context.params.customerID;
-    if (!customerID) {
+    const userID = context.params.userID;
+    if (!userID) {
       throw new Error("No session ID specificed");
     }
 
-    const checkoutSession = await stripe.checkout.sessions.list({
-      customer: customerID,
-    });
-
-    return NextResponse.json(checkoutSession);
+    return NextResponse.json("");
   } catch (e: any) {
     throw new Error(e);
   }
