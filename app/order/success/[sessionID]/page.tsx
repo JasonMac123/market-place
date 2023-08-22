2bf83d49615e53254156cee2ea338ee2b9f21141
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-21 22:10:41 -0400

    Fixes double data entry when rerendering page twice

--- app/order/success/[sessionID]/page.tsx
+++ app/order/success/[sessionID]/page.tsx
@@ -18,8 +18,11 @@ const OrderPage = async ({
   const result = await axios.get(
     `http://localhost:3000/api/session/${params.sessionID}`
   );
-  // currently creates double entry in database due to double rerender
-  createOrder({ userID: searchParams.userID, sessionID: params.sessionID });
+
+  await createOrder({
+    userID: searchParams.userID,
+    sessionID: params.sessionID,
+  });
 
   if (!result.data) {
     return (
