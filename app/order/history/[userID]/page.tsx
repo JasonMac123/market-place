c6e5cf3261fc9242e17d4f3fcd0cde1a9e3b2231
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-24 14:51:18 -0400

    Creates component to pass data of order history to client side

--- app/order/history/[userID]/page.tsx
+++ app/order/history/[userID]/page.tsx
@@ -1,13 +1,22 @@
-import getOrdersByUserID from "@/app/firebase/getOrdersByUserID";
-import { UserParams } from "@/app/types/types";
 import axios from "axios";
+
+import { UserParams } from "@/app/types/types";
+
+import ClientContainer from "@/app/components/containers/ClientContainer";
+import StripeItemCard from "@/app/components/cards/StripeItemCard";
 
 const Page = async ({ params }: { params: UserParams }) => {
   const result = await axios.get(
     `http://localhost:3000/api/multiple-sessions/${params.userID}`
   );
 
-  return <div></div>;
+  return (
+    <div>
+      <ClientContainer>
+        <OrderHistoryContainer data={result.data} />
+      </ClientContainer>
+    </div>
+  );
 };
 
 export default Page;
