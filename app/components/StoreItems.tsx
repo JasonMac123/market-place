3bbf8602f1add89c7be5062f7efed4f86f8c51eb
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-29 15:46:22 -0400

    Fixes styling for store front item cards to include responsive design for mobile screens

--- app/components/StoreItems.tsx
+++ app/components/StoreItems.tsx
@@ -41,7 +41,7 @@ const StoreItems: React.FC<StoreItemsProps> = async ({ searchParams }) => {
   }
 
   return (
-    <div className="flex flex-wrap gap-16 w-4/5 mx-auto h-full bg-white p-16 rounded-md">
+    <div className="flex flex-col items-center lg:justify-center xl:justify-start lg:flex-row flex-wrap gap-8 w-4/5 mx-auto h-full bg-white p-16 rounded-md">
       {items.map((item: Item) => {
         return <ItemCard key={item.id} {...item} />;
       })}
