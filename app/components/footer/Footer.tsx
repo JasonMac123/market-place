058f0bbc316d5a017f49c76f3a03c590fe83f970
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-29 21:58:41 -0400

    Adds footer mobile css changes

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -17,47 +17,49 @@ const Footer = () => {
   return (
     <div className="w-full h-88 bg-lapis flex flex-col pt-12 justify-around text-alice mt-8">
       <div className="flex justify-around pb-8">
-        <div className="flex">
+        <div className="flex lg:flex-row flex-col">
           <Logo alt />
-          <div className="flex flex-col gap-6 justify-start">
-            <h3 className="text-4xl">Visit us at our physical stores</h3>
-            <StoreLocation
-              streetName="Steeles Avenue West"
-              streetNumber={4001}
-              city="Toronto"
-              region="Ontario"
-              postalCode="L4K 2Y2"
-            />
-            <StoreLocation
-              streetName="St George Street"
-              streetNumber={1659}
-              city="Vancouver"
-              region="British Columbia"
-              postalCode="V5T 1Z7"
-            />
-            <h3 className="text-white text-2xl">More coming soon!</h3>
-          </div>
-        </div>
-        <div>
-          <div className="text-4xl mb-2">Contact us!</div>
-          <div className="space-y-2">
-            <div className="flex gap-2 items-center">
-              <AiFillPhone size={20} />
-              <h3> (416) - 111 - 1111</h3>
+          <div className="flex md:flex-row space-y-4 md:space-y-0 flex-col justify-between mx-8 mt-4">
+            <div className="flex flex-col gap-6 mr-4 justify-start">
+              <h3 className="text-4xl">Visit us at our physical stores</h3>
+              <StoreLocation
+                streetName="Steeles Avenue West"
+                streetNumber={4001}
+                city="Toronto"
+                region="Ontario"
+                postalCode="L4K 2Y2"
+              />
+              <StoreLocation
+                streetName="St George Street"
+                streetNumber={1659}
+                city="Vancouver"
+                region="British Columbia"
+                postalCode="V5T 1Z7"
+              />
+              <h3 className="text-white text-xl">More coming soon!</h3>
             </div>
-            <div className="flex gap-2 items-center">
-              <MdEmail size={20} />
-              <h3> Jason@LookOfJapan.com</h3>
+            <div className="mr-4">
+              <div className="text-4xl mb-2">Contact us!</div>
+              <div className="space-y-2">
+                <div className="flex gap-2 items-center">
+                  <AiFillPhone size={20} />
+                  <h3> (416) - 111 - 1111</h3>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <MdEmail size={20} />
+                  <h3> Jason@LookOfJapan.com</h3>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <div className="text-4xl mb-4">Follow our social media!</div>
-          <div className="space-y-3">
-            <MediaLink Icon={AiFillFacebook} label="FaceBook" />
-            <MediaLink Icon={AiFillInstagram} label="Instagram" />
-            <MediaLink Icon={AiOutlineTwitter} label="Twitter" />
-            <MediaLink Icon={AiFillLinkedin} label="Linked In" />
+            <div className="flex flex-col">
+              <div className="text-4xl mb-4">Follow our social media!</div>
+              <div className="space-y-3">
+                <MediaLink Icon={AiFillFacebook} label="FaceBook" />
+                <MediaLink Icon={AiFillInstagram} label="Instagram" />
+                <MediaLink Icon={AiOutlineTwitter} label="Twitter" />
+                <MediaLink Icon={AiFillLinkedin} label="Linked In" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
