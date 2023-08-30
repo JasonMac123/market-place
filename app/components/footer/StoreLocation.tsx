14c71ce9ca5888d718be8572eadc447d7d110d93
JasonSnow123 <m.jason.2018@gmail.com>
2023-08-29 22:09:40 -0400

    Has street map locations different colour to show redirection to google maps location

--- app/components/footer/StoreLocation.tsx
+++ app/components/footer/StoreLocation.tsx
@@ -16,11 +16,11 @@ const StoreLocation: React.FC<StoreLocationProps> = ({
   postalCode,
 }) => {
   return (
-    <div>
-      <h4>
+    <div className="text-blue-700 hover:text-purple-900">
+      <h4 className=" hover:cursor-pointer ">
         {streetNumber}, {streetName}
       </h4>
-      <h4>
+      <h4 className="hover:cursor-pointer">
         {city}, {region} {postalCode}
       </h4>
     </div>
