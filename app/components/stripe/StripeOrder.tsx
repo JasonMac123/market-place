dd12e3a774eca7949779052e28761d75b57cc851
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-04 20:25:47 -0400

    Fixes styling of order header information component

--- app/components/stripe/StripeOrder.tsx
+++ app/components/stripe/StripeOrder.tsx
@@ -22,18 +22,22 @@ const StripeOrder: React.FC<StripeOrderProps> = ({
 
   return (
     <div className="w-full mx-auto border-[1px] border-black rounded-xl p-4 lg:p-8">
-      <div className="w-full bg-neutral-400 flex lg:flex-row flex-col 2xl:h-20 justify-between space-y-4 lg:space-y-0 lg:items-center p-4 rounded-xl overflow-hidden">
-        <div className="flex lg:flex-row flex-col w-full">
-          Order Number -
-          <div
-            className="hover:underline hover:cursor-pointer hover:text-celestial lg:ml-2 ml-0 break-all"
-            onClick={() => router.push(`/order/success/${orderID}`)}
-          >
-            {orderID}
+      <div className="w-full bg-neutral-300 flex flex-col 2xl:h-28 justify-between space-y-4 lg:space-y-0 lg:items-center p-4 rounded-xl overflow-hidden">
+        <div className="w-full flex space-x-4">
+          <div className="flex lg:flex-row flex-col w-full space-x-4">
+            Order Number -
+            <div
+              className="hover:underline hover:cursor-pointer hover:text-celestial lg:ml-2 ml-0 break-all"
+              onClick={() => router.push(`/order/success/${orderID}`)}
+            >
+              {orderID}
+            </div>
           </div>
+          <div>Ordered on {timeStamp}</div>
         </div>
-        <div>Ordered on {timeStamp}</div>
-        <div>Order total - ${total}</div>
+        <div className="w-full">
+          <h2 className="text-right">Order total - ${total}</h2>
+        </div>
       </div>
       <div className="grid grid-cols-1 2xl:grid-cols-2 w-full h-fit">
         {data.map((item) => {
