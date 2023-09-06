3a0d379dc61683459f14b50fa87656e613881c40
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-06 16:30:23 -0400

    Fixes centering of item cards on parent div

--- app/components/StoreItems.tsx
+++ app/components/StoreItems.tsx
@@ -41,7 +41,13 @@ const StoreItems: React.FC<StoreItemsProps> = async ({ searchParams }) => {
   }
 
   return (
-    <div className="flex flex-col items-center lg:justify-center xl:justify-start lg:flex-row flex-wrap gap-8 w-4/5 mx-auto h-full bg-white p-16 rounded-md">
+    <div className="flex flex-col justify-center md:flex-row flex-wrap gap-4 lg:gap-8 w-4/5 mx-auto h-fit bg-white px-2 py-8 lg:px-4 xl:p-8 rounded-md">
+      {items.map((item: Item) => {
+        return <ItemCard key={item.id} {...item} />;
+      })}
+      {items.map((item: Item) => {
+        return <ItemCard key={item.id} {...item} />;
+      })}
       {items.map((item: Item) => {
         return <ItemCard key={item.id} {...item} />;
       })}
