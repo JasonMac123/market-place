aac325e11f124eb9648ebfdaca16709344423a4b
JasonSnow123 <m.jason.2018@gmail.com>
2023-09-10 22:50:06 -0400

    Fixes bug where user menu would be off-screen

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className="fixed z-30 flex justify-between items-center bg-lapis w-full h-[20vh]">
       <Logo />
-      <h1 className="text-7xl invisible md:visible text-alice text-center">
+      <h1 className="text-7xl hidden md:visible text-alice text-center">
         Look of Japan
       </h1>
       <UserMenu />
